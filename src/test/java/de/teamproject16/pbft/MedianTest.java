dd49a88bb4915ce9e365c9dfd88d4be6710f8370
luckydonald <luckydonald@flutterb.at>
2017-01-26 02:40:01 +0100

    [java_node] Added a 3 item median test.

--- src/test/java/de/teamproject16/pbft/MedianTest.java
+++ src/test/java/de/teamproject16/pbft/MedianTest.java
@@ -57,4 +57,16 @@ public class MedianTest {
         initStore.add(init4);
         assertEquals(5.485434, Median.calculateMedian(initStore), 0);
     }
+
+    @Test
+    public void evunMoarTestCalculateMedian() throws Exception {
+        List<InitMessage> initStore = new LinkedList<>();
+        InitMessage init1 = new InitMessage(1,1,0);
+        InitMessage init2 = new InitMessage(1,2,0.4458);
+        InitMessage init3 = new InitMessage(1,3,2);
+        initStore.add(init1);
+        initStore.add(init2);
+        initStore.add(init3);
+        assertEquals(0.4458, Median.calculateMedian(initStore), 0);
+    }
 }
