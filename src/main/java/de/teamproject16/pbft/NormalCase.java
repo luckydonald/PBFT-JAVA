035b298cadcae9340dfd046d7709722ce1b17794
luckydonald <luckydonald@flutterb.at>
2017-03-26 23:06:37 +0200

    [node_java] Changed timeout to 5 seconds (was 1), so it has time for a .

--- src/main/java/de/teamproject16/pbft/NormalCase.java
+++ src/main/java/de/teamproject16/pbft/NormalCase.java
@@ -25,7 +25,7 @@ import static java.util.stream.Collectors.groupingBy;
  */
 
 public class NormalCase {
-    public int sequencelength = 1000;
+    public int sequencelength = 5000;
 
     int leader = 1;
 
