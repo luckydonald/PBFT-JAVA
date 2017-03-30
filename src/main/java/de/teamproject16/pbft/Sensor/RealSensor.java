a65758322eb30a04dcf2cce9c597fec478cc35f9
PlayingBacon <PlayingBacon@users.noreply.github.com>
2017-03-30 23:18:48 +0200

    [node_java] Moved DS1820Reader functionality to RealSensor

--- src/main/java/de/teamproject16/pbft/Sensor/RealSensor.java
+++ src/main/java/de/teamproject16/pbft/Sensor/RealSensor.java
@@ -1,16 +1,65 @@
 package de.teamproject16.pbft.Sensor;
 
+import de.luckydonald.utils.dockerus.IDoNotWantThisException;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
 /**
  * This emulates sensor data.
  */
 public class RealSensor extends FakeSensor {
+    ///sys/bus/w1/devices/XX-XXXX..../w1_slave
+	/* path to search for devices in filesystem */
+    private static String devicesPath = "/sys/bus/w1/devices";
+
+    /* file of the measured values */
+    private static String valueFile = "w1_slave";
+
+    /* id of sensor */
+    private static String id = null;
+
     public double value;
 
-    public RealSensor(double value) {
-        this.value = value;
+    public RealSensor() {
+        File searchPath = new File(devicesPath);
+        if (searchPath.listFiles()!=null) {
+            for (File f: searchPath.listFiles()) {
+                if (f.isDirectory() && !f.getName().startsWith("w1_bus_master"))
+                    id = f.getName();
+            }
+        }
+        this.id = id;
     }
 
-    public double getRealSensorValue(){
-        return this.value;
+    public double getRealSensorValue() throws IDoNotWantThisException {
+        Path path = FileSystems.getDefault().getPath(devicesPath, id, valueFile);
+        List<String> lines;
+
+        int attempts = 3;
+        boolean crcOK = false;
+
+        while (attempts > 0) {
+            try {
+                lines = Files.readAllLines(path);
+                for(String line: lines) {
+                    if (line.endsWith("YES"))
+                        crcOK = true;
+                    else if (line.matches(".*t=[0-9]+") && crcOK)
+                        return Integer.valueOf(line.substring(line.indexOf("=")+1))/1000.0;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                continue;
+            }
+            attempts--;
+        }
+
+        throw new IDoNotWantThisException("");
     }
+
+
 }
